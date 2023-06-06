91b48d49bc918ac75df198dbae1aa2c4d92f7639
Oscar <oscarj5647@gmail.com>
2023-06-05 20:49:02 -0500

    .

--- lib/Widgets/NavBar.dart
+++ lib/Widgets/NavBar.dart
@@ -158,11 +158,6 @@ class _NavBarState extends State<NavBar> {
       ),
     );
     MainPage.testform = form;
-    MainPage.items = [
-      "Prueba ${numero} ${option}",
-      "Prueba ${numero} ${option}",
-      "Prueba ${numero} ${option}",
-      "Prueba ${numero} ${option}"
-    ];
+
   }
 }
